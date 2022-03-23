1119b48d82046aa8a3b5d1cfc2179d8c9f354927
CamiloLo <100646247+CamiloLo@users.noreply.github.com>
2022-03-23 10:42:50 -0500

    Contador

--- 05/script.js
+++ 05/script.js
@@ -1,12 +1,10 @@
 const $contador = document.getElementById('contador');
-const $btnincrementar = document.getElementById('btnincrementar')
-const $btndecrementar = document.getElementById('btndecrementar')
+const $btnincrementar = document.getElementById('btnIncrementar')
+const $btndecrementar = document.getElementById('btnDecrementar')
 
-$contador.textContent = contador;
+let contador = 0 
 
-let contador = 0
-
-$contador.textContent = contador;
+$contador.textContent = contador ;
 
 function incrementar() {
   contador++;
@@ -19,7 +17,8 @@ function decrementar() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  $btnIncrementar.addEventListener('click', () => {
+  $btnincrementar.addEventListener('click', () => {
+    console.log('click')
     incrementar();
     if (contador > 0) {
       $contador.classList.remove('red');
@@ -30,42 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
       $contador.classList.remove('blue');
     }
   });
-  $btnDecrementar.addEventListener('click', () => {
-    decrementar();
-    if (contador < 0) {
-      $contador.classList.remove('blue');
-      $contador.classList.add('red');
-    }
-    if (contador === 0) {
-      $contador.classList.remove('red');
-      $contador.classList.remove('blue');
-    }
-  });
-});$contador.textContent = contador;
-
-function incrementar() {
-  contador++;
-  $contador.textContent = contador;
-}
-
-function decrementar() {
-  contador--;
-  $contador.textContent = contador;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  $btnIncrementar.addEventListener('click', () => {
-    incrementar();
-    if (contador > 0) {
-      $contador.classList.remove('red');
-      $contador.classList.add('blue');
-    }
-    if (contador === 0) {
-      $contador.classList.remove('red');
-      $contador.classList.remove('blue');
-    }
-  });
-  $btnDecrementar.addEventListener('click', () => {
+  $btndecrementar.addEventListener('click', () => {
     decrementar();
     if (contador < 0) {
       $contador.classList.remove('blue');
