d36da24435173638800d2ab3162c2d1189d1663f
CamiloLo <100646247+CamiloLo@users.noreply.github.com>
2022-04-06 13:49:24 -0500

    proceso de presentacion

--- 08/bootstrap/js/main.js
+++ 08/bootstrap/js/main.js
@@ -1,17 +1,39 @@
+
+const validCrendentials = {
+    username: 'celulares@gmail.com',
+    password: '12345678'
+};
+
+
 const $username = document.getElementById('inUsername')
 const $password = document.getElementById('inPassword')
 const $submit = document.getElementById('submitbtn')
 
-const handleUsername = () => {
-    console.log('inUsername')
+const myCrendentials = {
+    username: null,
+    password: null,
+};
+
+const handleUsername = (event) => {
+    //console.log('value', event.target.value)
+    myCrendentials.username = event.target.value
 }
 
-const handlePassword = () => {
-    console.log('inPassword')
+const handlePassword = (event) => {
+    //console.log('value', event.target.value)
+    myCrendentials.password = event.target.value
 }
 
 const handlesubmit = () => {
-    console.log('submitbtn')
+    const username = myCrendentials.username === validCrendentials.username;
+    //console.log('username')
+    const password = myCrendentials.password === validCrendentials.password;
+    //console.log('password')
+    if (username && password) {
+        console.log('permitir acceso')
+    } else {
+        console.log('denegar acceso')
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
