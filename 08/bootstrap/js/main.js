d6d969aab001a9745b5e770adc99c0aca82b897a
CamiloLo <100646247+CamiloLo@users.noreply.github.com>
2022-04-07 10:05:54 -0500

    Contenido Privado

--- 08/bootstrap/js/main.js
+++ 08/bootstrap/js/main.js
@@ -1,6 +1,6 @@
 
 const validCrendentials = {
-    username: 'celulares@gmail.com',
+    username: 'camilo@gmail.com',
     password: '12345678'
 };
 
@@ -8,29 +8,30 @@ const validCrendentials = {
 const $username = document.getElementById('inUsername')
 const $password = document.getElementById('inPassword')
 const $submit = document.getElementById('submitbtn')
-
+const $private = document.getElementById('private')
 const myCrendentials = {
     username: null,
     password: null,
 };
 
-const handleUsername = (event) => {
-    //console.log('value', event.target.value)
+const handleUsername = (event) => {    
     myCrendentials.username = event.target.value
 }
 
 const handlePassword = (event) => {
-    //console.log('value', event.target.value)
     myCrendentials.password = event.target.value
 }
 
-const handlesubmit = () => {
+const handlesubmit = (e) => {
+    e.preventDefault()
     const username = myCrendentials.username === validCrendentials.username;
-    //console.log('username')
+    console.log(username)
     const password = myCrendentials.password === validCrendentials.password;
-    //console.log('password')
+    console.log(password)
     if (username && password) {
         console.log('permitir acceso')
+        $private.classList.remove('disabled')
+
     } else {
         console.log('denegar acceso')
     }
