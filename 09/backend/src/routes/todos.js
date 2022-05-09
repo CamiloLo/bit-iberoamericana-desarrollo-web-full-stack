62d814f8943f880cc1a6499534d20dd34218216f
CamiloLo <100646247+CamiloLo@users.noreply.github.com>
2022-05-08 22:38:57 -0500

    API

--- 09/backend/src/routes/todos.js
+++ 09/backend/src/routes/todos.js
@@ -1,27 +1,48 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
 const todoSchema = require('../models/todos')
 
 router.post('/todos', (request, response) => {
-    response.send('Crear Tarea');
     console.log('request:', request.body)
-    //const newTodo = 
+    //response.send('Crear Tarea');
+    const newTodo = todoSchema(request.body)
+    newTodo.save()
+    .then((data)=>response.json({success: data})).catch((err)=>response.json(error))
   });
 
+
   router.get('/todos', (request, response) => {
-    response.send('Leer todas las tareas');
+    //response.send('Leer todas las tareas');
+    todoSchema
+    .find()
+    .then((data)=>response.json({success: data})).catch((err)=>response.json(error))
   });
   
   router.get('/todos/:id', (request, response) => {
-    response.send('Leer una tarea especifica');
+    //response.send('Leer una tarea especifica');
+    const{ id } = request.params;
+    todoSchema
+    .findById(id)
+    .then((data)=>response.json({success: data})).catch((err)=>response.json(error))
   });
 
   router.put('/todos/:id', (request, response) => {
-    response.send('Actualizar una tarea');
+    //response.send('Actualizar una tarea');
+    const{ id } = request.params;
+    const todo = request.body
+    todoSchema
+    .updateOne({_id: id}, {$set: todo})
+    .then((data)=>response.json({success: data})).catch((err)=>response.json(error))
+
   });
 
   router.delete('/todos/:id', (request, response) => {
-    response.send('Elimina una tarea');
+    //response.send('Elimina una tarea');
+    const{id} = request.params;
+    todoSchema
+    .deleteOne({_id: id})
+    .then((data)=>response.json({success: data})).catch((err)=>response.json(error))
   });
 
   module.exports = router;
--- 09/backend/src/server.js
+++ 09/backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors= require('cors')
 const mongoose = require('mongoose')
 const dotenv= require('dotenv')
 const todosRouter = require('./routes/todos')
@@ -6,16 +7,17 @@ const todosRouter = require('./routes/todos')
 const server =  express()
 const port = 4000
 
-
-server.use('/api/v1', todosRouter)
+server.use(cors())
 server.use(express.json())
+server.use('/api/v1', todosRouter)
+
 dotenv.config();
 
 
 mongoose
 .connect(process.env.MONGODB_URI)
 .then(()=> console.log('conectado a la Base de datos') )
-  .catch(()=> console.log('ERROR AL CONECTAR A LA BASE DE DATOS'))
+.catch(()=> console.log('ERROR AL CONECTAR A LA BASE DE DATOS'))
 
 
 
@@ -23,6 +25,6 @@ mongoose
     response.send('Hola desde la raíz');
   });
 
-server.listen(port, ()=>{(console.log(`Servidor corriendo en localhost, en el puerto ${port}`)
-)} )
-2
+  server.listen(port, () => {
+    console.log(`Servidor corriendo en localhost, en el puerto ${port}`);
+  });
